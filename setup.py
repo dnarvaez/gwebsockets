777954a3a97de2e62817f3180d5b3bc530caff65
Daniel Narvaez <dwnarvaez@gmail.com>
2014-01-02 22:29:08 +0000

    Release 0.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class LintCommand(Command):
 
 setup(name="gwebsockets",
       packages=["gwebsockets"],
-      version="0.3",
+      version="0.4",
       description="GLib based websockets server",
       long_description=long_description,
       author="Daniel Narvaez",
